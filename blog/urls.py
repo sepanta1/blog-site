eeda12937bfbc6536ed9a4ac7fd251159a51e1b4
sepanta <145872489+sepanta1@users.noreply.github.com>
2025-08-12 19:37:53 +0330

    added search
    +pagination

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ app_name = 'blog'
 urlpatterns = [
     path('blog-home/', views.blog_home, name='blog-home'),
     path('<int:pid>/', views.blog_single, name='blog-single'),
-    path('category/<str:cat_name>',views.category_link, name='cat_link'),
+    path('category/<str:cat_name>',views.blog_home, name='cat_link'),
+    path('author/<str:author_name>',views.blog_home,name='author_name'),
+    path('search/',views.blog_search,name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 from django.utils.timesince import timesince
 from django.http import Http404
 from django.utils.timezone import now
 
 
-def blog_home(request):
-    post = Post.objects.filter(status=True)
+def blog_home(request, cat_name=None, author_name=None):
+    post = Post.objects.filter(status=True).order_by('-published_date')
+    if cat_name:
+        post = post.filter(
+            category__name=cat_name)
+    if author_name:
+        post = post.filter(
+            author__username=author_name)
     if not post.exists():
         raise Http404("No posts found")
-    context = {'post': post, }
+    p = Paginator(post, 2)
+    page_number = request.GET.get('page')
+    post = p.get_page(page_number)  # returns the desired page object
+
+    context = {'post':post}
     return render(request, 'blog/blog-home.html', context)
 
 
@@ -17,13 +28,23 @@ def blog_single(request, pid):
 
     post = get_object_or_404(Post, pk=pid, status=1)
 
-    next_post = Post.objects.filter(status=1, pk__gt=pid).order_by('pk').first()
-    prev_post = Post.objects.filter(status=1, pk__lt=pid).order_by('-pk').first()
+    next_post = Post.objects.filter(
+        status=1, pk__gt=pid).order_by('pk').first()
+    prev_post = Post.objects.filter(
+        status=1, pk__lt=pid).order_by('-pk').first()
     context = {'post': post, 'next_post': next_post, 'prev_post': prev_post, }
     return render(request, 'blog/blog-single.html', context)
 
 
-def category_link(request, cat_name):
+def blog_category(request, cat_name):
     post = Post.objects.filter(status=1).filter(category__name=cat_name)
+    context = {'post': post}
+    return render(request, 'blog/blog-home.html', context)
+
+
+def blog_search(request):
+    post = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        post = post.filter(content__icontains=request.GET.get('s'))
     context = {'post': post}
     return render(request, 'blog/blog-home.html', context)
