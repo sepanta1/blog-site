4d3446a351fb4b6daf2b5097d22136109ba1e09a
sepanta <145872489+sepanta1@users.noreply.github.com>
2026-08-07 12:27:55 +0330

    fix: duplicate blog URL includes

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,11 @@
-
-# Import debug toolbar URLs (only active in DEBUG mode)
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+
+# Import debug toolbar URLs (only active in DEBUG mode)
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 from blog.sitemaps import BlogSitemap
 
@@ -19,7 +19,7 @@ sitemaps = {
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("website.urls")),
-    path("api/v1/", include("blog.api_urls")),
+    
     path("blog/", include("blog.urls")),
     path("accounts/", include("accounts.urls")),
     # robots module
@@ -35,7 +35,7 @@ urlpatterns = [
     # tinymce editor module
     path("tinymce/", include("tinymce.urls")),
     # api v1
-    path("api/v1/", include("blog.urls")),
+    path("api/v1/", include("blog.api_urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
